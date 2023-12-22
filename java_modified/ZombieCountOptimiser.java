eb6d349619de85eef53a1e275702af978903117b
m0d1nst4ll3r <92337440+m0d1nst4ll3r@users.noreply.github.com>
2023-12-22 06:12:34 +0100

    Add files via upload

---
+++ java_modified/ZombieCountOptimiser.java
@@ -0,0 +1,14 @@
+package zombie.popman;
+
+import java.util.ArrayList;
+import zombie.characters.IsoZombie;
+
+public class ZombieCountOptimiser {
+   public static final ArrayList zombiesForDelete = new ArrayList();
+
+   public static void startCount() {
+   }
+
+   public static void incrementZombie(IsoZombie var0) {
+   }
+}
